d7ca980163254b4503a92180cce7bc4b42bb52f9
Simo Mos <sorariku95@hotmail.gr>
2021-08-21 22:01:27 +0300

    update gradle from 4.2.2 to 7.0.1

--- app/src/main/java/com/mosc/simo/ptuxiaki3741/backend/file/extensions/gml2/GML2Reader.java
+++
@@ -1,5 +0,0 @@
-package com.mosc.simo.ptuxiaki3741.backend.file.extensions.gml2;
-
-public class GML2Reader {
-    //TODO: gml2 reader
-}
--- app/src/main/java/com/mosc/simo/ptuxiaki3741/backend/file/extensions/gml3v1/GML3v1Reader.java
+++
@@ -1,5 +0,0 @@
-package com.mosc.simo.ptuxiaki3741.backend.file.extensions.gml3v1;
-
-public class GML3v1Reader {
-    //TODO: gml3 reader
-}
